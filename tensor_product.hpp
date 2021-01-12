2dad6a04d43d8750d55e32608f937c8c505e2d55
Sean McBane <sean.mcbane@protonmail.com>
2021-01-12 12:10:15 -0600

    Edited tensor product for clarity

--- tensor_product.hpp
+++ tensor_product.hpp
@@ -19,21 +19,12 @@ void evaluate_tensor_product(U &grad, const V &v, const ModelInfo<MeshType<1>> &
     grad.fill(0);
     const auto &refmats = minfo.ref_stiffness_matrices;
     Eigen::Matrix<double, ModelInfo<MeshType<1>>::el_stiffness_matrix_t::RowsAtCompileTime, 1> uel, vel;
-    Eigen::Matrix<double, ModelInfo<MeshType<1>>::el_stiffness_matrix_t::RowsAtCompileTime, Eigen::Dynamic>
-        dKdrhou;
-    Eigen::VectorXd weights;
 
     for (std::size_t i = 0; i < minfo.mesh.num_elements(); ++i)
     {
         double dsdrho = minfo.interp.derivative(minfo.rho_filt[i]);
         const auto &Kel = refmats[i];
         const auto &efilt = minfo.filter[i];
-
-        weights.resize(efilt.entries.size());
-        for (int i = 0; i < weights.size(); ++i)
-        {
-            weights[i] = efilt.entries[i].weight;
-        }
 
         const auto &nn = minfo.mesh.element(i).node_numbers();
         for (std::size_t j = 0; j < nn.size(); ++j)
@@ -43,11 +34,11 @@ void evaluate_tensor_product(U &grad, const V &v, const ModelInfo<MeshType<1>> &
             uel[2 * j + 1] = minfo.displacement[2 * nn[j] + 1];
             vel[2 * j + 1] = v[2 * nn[j] + 1];
         }
-        dKdrhou = dsdrho * (Kel * uel) * weights.transpose();
-        assert(dKdrhou.cols() == weights.size());
-        for (int i = 0; i < weights.size(); ++i)
+
+        const double vTdKdrhou = dsdrho * vel.dot(Kel * uel);
+        for (unsigned i = 0; i < efilt.entries.size(); ++i)
         {
-            grad[efilt.entries[i].index] += vel.transpose() * dKdrhou.col(i);
+            grad[efilt.entries[i].index] += efilt.entries[i].weight * vTdKdrhou;
         }
     }
 }
